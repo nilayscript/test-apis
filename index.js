2ef124e1c4909f0a6a8b4206410d7b2155a12710
nilayscript <nilay.singh@digisigns.in>
2025-12-09 14:10:33 +0530

    Update

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.json({ status: "ok", message: "API is running" });
+});
+
 const humans = {
   1: {
     id: 1,
